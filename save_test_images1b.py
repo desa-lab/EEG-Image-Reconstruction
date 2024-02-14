2aff3dc8472b82411aa15cba8d795705c664ff94
simonfei123 <tfei@ucsd.edu>
2024-02-14 07:02:46 +0000

    create averaged test trial reconstruction images comparison video

--- save_test_images1b.py
+++ save_test_images1b.py
@@ -4,9 +4,12 @@ from PIL import Image
 
 #The same for all subjects
 # images = np.load('data/processed_data/subj01/nsd_test_stim_sub1.npy')
-images = np.load('cache/processed_data/BIGMEG1/test_images1b_sub-BIGMEG1.npy', mmap_mode='r')
 
-test_images_dir = 'cache/thingsmeg_stimuli/test_images1b/'
+# images = np.load('cache/processed_data/BIGMEG1/test_images1b_sub-BIGMEG1.npy', mmap_mode='r')
+# test_images_dir = 'cache/thingsmeg_stimuli/test_images1b/'
+
+images = np.load('cache/processed_data/BIGMEG1/test_avg_images1b_sub-BIGMEG1.npy', mmap_mode='r')
+test_images_dir = 'cache/thingsmeg_stimuli/avg_test_images1b/'
 
 if not os.path.exists(test_images_dir):
    os.makedirs(test_images_dir)
